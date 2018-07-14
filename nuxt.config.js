747c0154827ffdb58f48f9fd2f1871397c2f2c06
Randy Téllez Galán <kronhyx@gmail.com>
2018-07-14 10:14:35 +0200

    using static folder instead assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt',
+    title: 'Inspinia',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
@@ -11,7 +11,19 @@ module.exports = {
       {hid: 'author', name: 'author', content: 'Kronhyx'}
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: '/css/bootstrap.min.css'},
+      {rel: 'stylesheet', href: '/font-awesome/css/font-awesome.css'},
+      {rel: 'stylesheet', href: '/css/animate.css'},
+      {rel: 'stylesheet', href: '/css/style.css'}
+    ],
+    script: [
+      {rel: 'script', href: '/js/jquery-2.1.1.js'},
+      {rel: 'script', href: '/js/bootstrap.min.js'},
+      {rel: 'script', href: '/js/plugins/metisMenu/jquery.metisMenu.js'},
+      {rel: 'script', href: '/js/plugins/slimscroll/jquery.slimscroll.min.js'},
+      {rel: 'script', href: '/js/inspinia.js'},
+      {rel: 'script', href: '/js/plugins/wow/wow.min.js'},
     ]
   },
   /*
