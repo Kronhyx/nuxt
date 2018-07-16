a4cb1e03c22171d868921dd8e4ec3d088a03f16b
Randy Téllez Galán <kronhyx@gmail.com>
2018-07-16 11:50:25 -0400

    add inspinia files

---
+++ layouts/landing.vue
@@ -0,0 +1,21 @@
+<template>
+  <nuxt/>
+</template>
+
+<script>
+  export default {
+    name: 'landing',
+    head() {
+      return {
+        title: 'Landing Page',
+        bodyAttrs: {
+          class: 'landing-page'
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="middle-box text-center animated">
+    <h3 class="font-bold">This is page content</h3>
+    <div class="error-desc">
+      You can create here any grid layout you want. And any variation layout you imagine:) Check out
+      main dashboard and other site. It use many different layout.
+      <br><a href="#" class="btn btn-primary m-t">Dashboard</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {}
+  }
+</script>
+
+<style>
+
+</style>
