12a3f5d06721ebf510fdf85b1a8f5cf89f294675
Randy Téllez Galán <kronhyx@gmail.com>
2018-07-16 11:50:25 -0400

    add inspinia files

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,79 @@
 <template>
   <div>
-    <nuxt/>
+    <nuxt-loading/>
+    <div id="wrapper">
+      <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+          <sidebar-nx/>
+        </div>
+      </nav>
+
+      <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+          <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+              <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#">
+                <i class="fa fa-bars"></i>
+              </a>
+              <form role="search" class="navbar-form-custom" method="post" action="#">
+                <div class="form-group">
+                  <input type="text" v-model="query" placeholder="Search for something..." class="form-control"
+                         id="top-search">
+                </div>
+              </form>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+              <li>
+                <a href="#">
+                  <i class="fa fa-sign-out"></i> Log out
+                </a>
+              </li>
+            </ul>
+
+          </nav>
+        </div>
+        <div class="row wrapper border-bottom white-bg page-heading">
+          <div class="col-sm-4">
+            <h2>This is main title</h2>
+            <ol class="breadcrumb">
+              <li>
+                <a href="#">This is</a>
+              </li>
+              <li class="active">
+                <strong>Breadcrumb</strong>
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div class="wrapper wrapper-content animated fadeIn">
+          <nuxt/>
+        </div>
+        <div class="footer">
+          <div class="pull-right">10GB of <strong>250GB</strong> Free.
+          </div>
+          <div>
+            <strong>Copyright</strong> Example Company &copy; 2014-2015
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+  import SidebarNx from "../components/Sidebar";
+  import NuxtLoading from "../.nuxt/components/nuxt-loading";
+
+  export default {
+    components: {NuxtLoading, SidebarNx},
+    data() {
+      return {
+        query: null
+      }
+    }
+  }
+</script>
+
 <style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
---
+++ layouts/gray.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="animated fadeIn">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gray',
+    head() {
+      return {
+        bodyAttrs: {
+          class: 'gray-bg'
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
